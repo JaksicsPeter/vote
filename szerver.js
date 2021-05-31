f8a3516f7c3d342b2155c9e9495102854f1a9674
Jenő <jaksics.peter.18f@szlgbp.hu>
2021-05-31 13:04:54 +0200

    Ez így még fontosabb lett uMu

--- szerver.js
+++ szerver.js
@@ -1,10 +1,19 @@
-const express = require("express")
-const app = express()
+const { response } = require("express");
+const express = require("express");
+const app = express();
 /*app.get("/", function(req, res) {
     res.end("HELLO WORLD!!!")
-})
+});
 */
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+app.use(express.urlencoded());
+app.post("/szavazas", function(req, res){
+    console.log(req.body);
+
+    res.redirect('/');
+});
 
 
-app.listen(9000)
+
+app.listen(9000);
