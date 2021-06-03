e2fa50d78e0996ca7a235c425d2ec9b71fe07f48
Jenő <jaksics.peter.18f@szlgbp.hu>
2021-06-03 11:43:26 +0200

    Mongoose invázió oWo

--- szerver.js
+++ szerver.js
@@ -1,19 +1,47 @@
 const { response } = require("express");
+
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config.json");
+
+mongoose.connect(config.databaseURL);
+
 const app = express();
-/*app.get("/", function(req, res) {
-    res.end("HELLO WORLD!!!")
-});
-*/
+
 app.use(express.static("public"));
 
+const schema = new mongoose.Schema({
+    nev: String,
+    szavazatok: Number
+});
+
+const model = mongoose.model('Opciok',schema,'Opciok');
+
 app.use(express.urlencoded());
+
 app.post("/szavazas", function(req, res){
     console.log(req.body);
+    model.findOne({ nev: req.body.pizza }, function(err, doc){
+        if(doc) {
+            console.log(req.body.pizza + " már létezik");
+            doc.szavazatok++;
+            doc.save();
+            //változtatjuk
+        } else {
+            console.log(req.body.pizza + " még nem létezik");
+            new model({
+                nev: req.body.pizza,
+                szavazatok: 1
+            }).save();
+        }
+    });
 
     res.redirect('/');
 });
 
 
-
+/*app.get("/", function(req, res) {
+    res.end("HELLO WORLD!!!")
+});
+*/
 app.listen(9000);
